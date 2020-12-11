68d0755e2570ef920911da5b553c74fc55f04968
Ramnath <ramnath2609@gmail.com>
2020-12-11 09:33:36 +0530

    initial

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAz4ZWZwkSnJ9ojibv1olnueaaEN6kitM8",
+  authDomain: "facebook-clone-b9d29.firebaseapp.com",
+  projectId: "facebook-clone-b9d29",
+  storageBucket: "facebook-clone-b9d29.appspot.com",
+  messagingSenderId: "169189810863",
+  appId: "1:169189810863:web:8e05b8d9c726f1cd1daa5a",
+};
